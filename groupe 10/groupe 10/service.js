b737cc812fe7a7c19b63549e1fc397e16e70e0dc
elmagnifiko <121030644+elmagnifiko@users.noreply.github.com>
2023-10-11 15:50:47 +0100

    Delete groupe 10 directory

--- groupe 10/groupe 10/service.js
+++
@@ -1,7 +0,0 @@
-let image = document.querySelector('.icon-avatar')
-const profilImage = JSON.parse(localStorage.getItem('profilImage')) ;
-
-if (profilImage) {
-    image.style.borderRadius = "250px"
-    image.src =  profilImage;
-}
